3c44ba15569d3e819760ade4c0bde1ed591e2538
MilenaSchmidt <milenaschmidt@protonmail.com>
2020-03-30 11:34:50 +0200

    User Roles

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -37,6 +37,11 @@ class User implements UserInterface
      */
     private $confirmed_at;
 
+    /** 
+     * @ORM\Column(type="json") 
+     */
+    private $roles = [];
+
     public function getId(): ?int
     {
         return $this->id;
@@ -90,13 +95,22 @@ class User implements UserInterface
         return $this;
     }
 
-    public function getRoles()
-    {
+    public function getRoles(): array
+    {   
         $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
-    
+
         return array_unique($roles);
+    }
+
+    public function setRoles(array $roles)
+    {
+        $this->roles = $roles;
+    }
+
+    public function resetRoles()
+    {
+        $this->roles = [];
     }
 
     /**
