5f19587eaf022b6a61f6c63c9a17c0ec6e113533
MilenaSchmidt <milenaschmidt@protonmail.com>
2020-04-01 15:20:49 +0200

    Category: model, migration and repository

---
+++ src/Entity/Category.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
+ */
+class Category
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\Configuration", mappedBy="category_id", cascade={"persist", "remove"})
+     */
+    private $configuration;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getConfiguration(): ?Configuration
+    {
+        return $this->configuration;
+    }
+
+    public function setConfiguration(Configuration $configuration): self
+    {
+        $this->configuration = $configuration;
+
+        // set the owning side of the relation if necessary
+        if ($configuration->getCategoryId() !== $this) {
+            $configuration->setCategoryId($this);
+        }
+
+        return $this;
+    }
+}
